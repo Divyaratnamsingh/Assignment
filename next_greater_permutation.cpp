4a785357a3b6038930ab292955cf5ecb13edc14c
Divyaratnamsingh <sdivyaratnam@gmail.com>
2025-04-16 16:21:29 +0530

    Update next_greater_permutation.cpp

--- next_greater_permutation.cpp
+++ next_greater_permutation.cpp
@@ -1,60 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+def next_permutation(nums):
+    n = len(nums)
+    k = -1
 
-void nextPermutation(vector<int>& nums) {
-    int n = nums.size();
-    int k = -1;
+    # Find the largest index k such that nums[k] < nums[k + 1]
+    for i in range(n - 2, -1, -1):
+        if nums[i] < nums[i + 1]:
+            k = i
+            break
 
-    // Find the largest index k such that nums[k] < nums[k + 1]
-    for (int i = n - 2; i >= 0; i--) {
-        if (nums[i] < nums[i + 1]) {
-            k = i;
-            break;
-        }
-    }
+    # If no such index is found, the current permutation is the largest
+    if k == -1:
+        nums.reverse()
+        return
 
-    // If no such index is found, the current permutation is the largest
-    if (k == -1) {
-        reverse(nums.begin(), nums.end());
-        return;
-    }
+    # Find the largest index l such that nums[k] < nums[l]
+    for i in range(n - 1, k, -1):
+        if nums[i] > nums[k]:
+            l = i
+            break
 
-    // Find the largest index l such that nums[k] < nums[l]
-    int l = -1;
-    for (int i = n - 1; i > k; i--) {
-        if (nums[i] > nums[k]) {
-            l = i;
-            break;
-        }
-    }
+    # Swap nums[k] and nums[l]
+    nums[k], nums[l] = nums[l], nums[k]
 
-    // Swap nums[k] and nums[l]
-    swap(nums[k], nums[l]);
+    # Reverse the sequence from nums[k + 1] to the end
+    nums[k + 1:] = reversed(nums[k + 1:])
 
-    // Reverse the sequence from nums[k + 1] to the end
-    reverse(nums.begin() + k + 1, nums.end());
-}
+def main():
+    n = int(input("Enter the number of elements: "))
+    nums = list(map(int, input("Enter the elements: ").split()))
 
-int main() {
-    vector<int> nums;
-    int n, temp;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> temp;
-        nums.push_back(temp);
-    }
+    next_permutation(nums)
 
-    nextPermutation(nums);
-    
-    cout << "The next permutation is: ";
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
+    print("The next permutation is:", *nums)
 
-    return 0;
-}
+if __name__ == "__main__":
+    main()
