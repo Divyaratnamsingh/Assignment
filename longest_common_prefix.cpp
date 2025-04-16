99273c70342c81ee61781268a42eabc870db468c
Divyaratnamsingh <sdivyaratnam@gmail.com>
2025-04-16 12:43:35 +0530

    Update longest_common_prefix.cpp

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -1,47 +1,31 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-string longestCommonPrefix(const vector<string>& strs) {
-    if (strs.empty()) {
-        return ""; // If the list is empty, there's no common prefix
-    }
-
-    // Take the first string as the reference
-    string prefix = strs[0];
-
-    // Compare the prefix with all other strings
-    for (int i = 1; i < strs.size(); i++) {
-        // Compare the current string with the prefix
-        while (strs[i].find(prefix) != 0) {  // While the prefix is not a prefix of the current string
-            prefix = prefix.substr(0, prefix.length() - 1); // Reduce the prefix by one character
-            if (prefix.empty()) {
-                return ""; // If no common prefix exists
-            }
-        }
-    }
-
-    return prefix;
-}
-
-int main() {
-    int n;
-    cout << "Enter the number of strings: ";
-    cin >> n;
-
-    vector<string> strs(n);
-    cout << "Enter " << n << " strings:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> strs[i];
-    }
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
     
-    string result = longestCommonPrefix(strs);
+    prefix = strs[0]
     
-    if (result.empty()) {
-        cout << "There is no common prefix." << endl;
-    } else {
-        cout << "The longest common prefix is: " << result << endl;
-    }
+    for i in range(1, len(strs)):
+        while not strs[i].startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    
+    return prefix
 
-    return 0;
-}
+def main():
+    n = int(input("Enter the number of strings: "))
+    strs = []
+
+    print(f"Enter {n} strings:")
+    for _ in range(n):
+        strs.append(input())
+
+    result = longest_common_prefix(strs)
+    
+    if not result:
+        print("There is no common prefix.")
+    else:
+        print("The longest common prefix is:", result)
+
+if __name__ == "__main__":
+    main()
