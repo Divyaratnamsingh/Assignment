aeac0261f5819996cc5a4bf9ece9ef627899b466
Divyaratnamsingh <sdivyaratnam@gmail.com>
2025-04-16 16:22:44 +0530

    Update palindrome_integer.cpp

--- palindrome_integer.cpp
+++ palindrome_integer.cpp
@@ -1,37 +1,29 @@
-#include <iostream>
-using namespace std;
+def is_palindrome(num):
+    # Edge case: negative numbers are not palindromes
+    if num < 0:
+        return False
 
-bool isPalindrome(int num) {
-    // Edge case: negative numbers are not palindromes
-    if (num < 0) return false;
+    # Single digit numbers are always palindromes
+    if 0 <= num < 10:
+        return True
 
-    // Edge case: single digit numbers are always palindromes
-    if (num >= 0 && num < 10) return true;
+    original = num
+    reversed_half = 0
 
-    int original = num;
-    int reversedHalf = 0;
+    # Reverse the second half of the number
+    while num > reversed_half:
+        reversed_half = reversed_half * 10 + num % 10
+        num //= 10
 
-    // Reverse the second half of the number
-    while (num > reversedHalf) {
-        reversedHalf = reversedHalf * 10 + num % 10;
-        num /= 10;
-    }
+    # Check for both even and odd length numbers
+    return num == reversed_half or num == reversed_half // 10
 
-    // If the number has an odd number of digits
-    // ignore the middle digit by dividing reversedHalf by 10
-    return num == reversedHalf || num == reversedHalf / 10;
-}
+def main():
+    num = int(input("Enter a number: "))
+    if is_palindrome(num):
+        print(f"{num} is a palindrome.")
+    else:
+        print(f"{num} is not a palindrome.")
 
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-
-    if (isPalindrome(num)) {
-        cout << num << " is a palindrome.\n";
-    } else {
-        cout << num << " is not a palindrome.\n";
-    }
-
-    return 0;
-}
+if __name__ == "__main__":
+    main()
