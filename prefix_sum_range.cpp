97b4ee4feab99e01ab299015924cb26d55ce84f0
Divyaratnamsingh <sdivyaratnam@gmail.com>
2025-04-16 16:23:54 +0530

    Update prefix_sum_range.cpp

--- prefix_sum_range.cpp
+++ prefix_sum_range.cpp
@@ -1,33 +1,25 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+def prefix_sum(arr):
+    n = len(arr)
+    prefix = [0] * n
+    prefix[0] = arr[0]
+    for i in range(1, n):
+        prefix[i] = prefix[i - 1] + arr[i]
+    return prefix
 
-vector<int> prefix_sum(const vector<int>& arr) {
-    int n = arr.size();
-    vector<int> prefix(n);
-    prefix[0] = arr[0];
-    for (int i = 1; i < n; ++i) {
-        prefix[i] = prefix[i-1] + arr[i];
-    }
-    return prefix;
-}
+def range_sum(prefix, L, R):
+    if L == 0:
+        return prefix[R]
+    else:
+        return prefix[R] - prefix[L - 1]
 
-int range_sum(const vector<int>& prefix, int L, int R) {
-    if (L == 0) {
-        return prefix[R];
-    } else {
-        return prefix[R] - prefix[L-1];
-    }
-}
+def main():
+    arr = [2, 4, 7, 3, 8, 1]
+    L = 1
+    R = 4
+    prefix = prefix_sum(arr)
+    sum_in_range = range_sum(prefix, L, R)
 
-int main() {
-    vector<int> arr = {2, 4, 7, 3, 8, 1};
-    int L = 1;
-    int R = 4;
-    vector<int> prefix = prefix_sum(arr);
+    print(f"Sum of elements from index {L} to {R} is: {sum_in_range}")
 
-    int sum_in_range = range_sum(prefix, L, R);
-
-    cout << "Sum of elements from index " << L << " to " << R << " is: " << sum_in_range << endl;
-    return 0;
-}
+if __name__ == "__main__":
+    main()
