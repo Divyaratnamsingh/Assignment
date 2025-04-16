a37c195d4b45c4243f992a608744af105e337896
Divyaratnamsingh <sdivyaratnam@gmail.com>
2025-04-16 11:58:36 +0530

    Update count_1_binary.py

--- count_1_binary.cpp
+++ count_1_binary.cpp
@@ -1,33 +1,21 @@
-#include <iostream>
-using namespace std;
+def count_ones_in_binary(num):
+    count = 0
+    while num > 0:
+        num = num & (num - 1)
+        count += 1
+    return count
 
-int countOnesInBinary(int num) {
-    int count = 0;
-    while (num > 0) {
-        // Remove the rightmost 1 bit
-        num = num & (num - 1);
-        count++;
-    }
-    return count;
-}
+def count_total_ones(n):
+    total_count = 0
+    for i in range(n + 1):
+        total_count += count_ones_in_binary(i)
+    return total_count
 
-// Function to count the total number of 1s in binary representation from 0 to n
-int countTotalOnes(int n) {
-    int totalCount = 0;
-    for (int i = 0; i <= n; i++) {
-        totalCount += countOnesInBinary(i);
-    }
-    return totalCount;
-}
+def main():
+    n = int(input("Enter a number n: "))
+    result = count_total_ones(n)
+    print(f"The total number of 1s in binary representation from 0 to {n} is: {result}")
 
-int main() {
-    int n;
-    cout << "Enter a number n: ";
-    cin >> n;
+if __name__ == "__main__":
+    main()
 
-    int result = countTotalOnes(n);
-
-    cout << "The total number of 1s in binary representation from 0 to " << n << " is: " << result << endl;
-
-    return 0;
-}
